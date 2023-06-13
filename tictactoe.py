29f76a86b21d25a86c606ca26e6f7efdc9efca9e
Vrishali2212 <vrish2212@gmail.com>
2023-06-12 18:10:10 -0700

    added printBoard funcion in tic tac toe

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,8 @@
 print "Welcome to the tictactoe game "
+
+gameboard = [1, '','','','','','','','','','']
+
+def printboard(myboard):
+	print(myboard[1]+" | " +myboard[2]+" | "+myboard[3])
+	print(myboard[4]+" | " +myboard[5]+" | "+myboard[6])
+	print(myboard[7]+" | " +myboard[8]+" | "+myboard[9])
