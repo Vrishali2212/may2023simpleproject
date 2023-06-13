334453ce2729f27212f5ef1f5c2ccc4ae6babc1a
Vrishali2212 <vrish2212@gmail.com>
2023-06-13 10:25:59 -0700

    calling printboard funciton in tic tac toe

--- tictactoe.py
+++ tictactoe.py
@@ -6,3 +6,7 @@ def printboard(myboard):
 	print(myboard[1]+" | " +myboard[2]+" | "+myboard[3])
 	print(myboard[4]+" | " +myboard[5]+" | "+myboard[6])
 	print(myboard[7]+" | " +myboard[8]+" | "+myboard[9])
+
+
+
+printboard(gameboard)
